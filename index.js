90012dfd552ddaab9860e4a3e9253353a913d9c0
Chely Ho <adept_inept@yahoo.com>
2017-11-10 02:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 53a5fdb330538418b6a51cddeb0affd40c41bb8a
